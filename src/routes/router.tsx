c6206d9e49a7f5c484bf6c566573c9702b71e421
Executor-create <just4dotaalex@gmail.com>
2026-04-03 20:25:26 +0300

    feat: add routes for ForgotPassword, ResetPassword, GamesPage, and GameDetail components

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,6 +5,10 @@ import Login from '../pages/Login';
 import { Profile } from '../pages/Profile';
 import RequireAuth from '../components/RequireAuth';
 import OTP from '../pages/OTP';
+import ForgotPasswordPage from '../pages/ForgotPassword';
+import ResetPasswordPage from '../pages/ResetPassword';
+import { GamesPage } from '../pages/Games';
+import GameDetail from '../pages/GameDetail';
 
 const router = createBrowserRouter([
   {
@@ -12,8 +16,24 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
+    path: '/forgot-password',
+    element: <ForgotPasswordPage />,
+  },
+  {
     path: '/otp',
     element: <OTP />,
+  },
+  {
+    path: '/reset-password',
+    element: <ResetPasswordPage />,
+  },
+  {
+    path: '/games',
+    element: <GamesPage />,
+  },
+  {
+    path: '/games/:id',
+    element: <GameDetail />,
   },
   {
     element: (
